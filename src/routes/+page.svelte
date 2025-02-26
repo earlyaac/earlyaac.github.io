0ab455e62fcfdd84f7635341fba7f33419de6b97
dahnte <cardenasdanten@gmail.com>
2025-02-25 22:00:43 -0800

    Button test

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import Button from '$lib/Button.svelte';
+	import slip from '$lib/slip.mp3';
+
+	let audio:HTMLAudioElement;
+
+	function honk() {
+		audio.load();
+		audio.play();
+	}
+</script>
+
+<audio src={slip} bind:this={audio}></audio>
+<Button onclick={honk} />
