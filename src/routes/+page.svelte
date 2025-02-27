ef27cddd96c950da89f42cf049b8a66d1f6f53aa
dahnte <cardenasdanten@gmail.com>
2025-02-26 20:18:27 -0800

    Added prerender

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Button from '$lib/Button.svelte';
+	import { version } from '$app/environment'
 
 	let audio:HTMLAudioElement;
 
@@ -14,8 +15,8 @@
 	<Button bind:audioBind={audio} audioSrc="https://cdn.freesound.org/previews/61/61847_142047-lq.mp3" buttonClick={honk} buttonText="Head"/>
 	<Button bind:audioBind={audio} audioSrc="https://cdn.freesound.org/previews/61/61847_142047-lq.mp3" buttonClick={honk} buttonText="Shoulder"/>
 	<Button bind:audioBind={audio} audioSrc="https://cdn.freesound.org/previews/61/61847_142047-lq.mp3" buttonClick={honk} buttonText="Knees"/>
-	<div class="col-span-3">
-		JUST A TEST OF <em>EARLY AAC</em>. WORK IN PROGRESS!
+	<div class="col-span-3 text-center">
+		EARLY AAC<br>{version}<br>WORK IN PROGRESS!
 	</div>
 	<Button bind:audioBind={audio} audioSrc="https://cdn.freesound.org/previews/61/61847_142047-lq.mp3" buttonClick={honk} buttonText="Toes"/>
 	<Button bind:audioBind={audio} audioSrc="https://cdn.freesound.org/previews/61/61847_142047-lq.mp3" buttonClick={honk} buttonText="This is"/>
